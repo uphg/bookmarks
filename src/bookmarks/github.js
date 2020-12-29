d2a3d4616eada45b20a862b0d758486d15f5ede5
ChenNing02 <2633565885@qq.com>
2020-12-29 11:46:21 +0800

    first commit

---
+++ src/bookmarks/github.js
@@ -0,0 +1,44 @@
+/* === GitHub 书签 === */
+
+export default {
+  title: 'GitHub',
+  id: 0,
+  list: [
+    {
+      title: '前端移动端开发工具',
+      description: '-',
+      img: '-',
+      href: 'https://github.com/hoosin/mobile-web-favorites',
+    },
+    {
+      title: '「方应杭」收集优质的中文前端博客',
+      description: '博主的公司信息来自网上公开资料，仅供参考，不保证准确性。',
+      img: '-',
+      href: 'https://github.com/FrankFang/best-chinese-front-end-blogs',
+    },
+    {
+      title: 'Vue 相关开源项目库集合',
+      description: '由 OpenDigg 整理并维护的 Vue 相关开源项目库集合。',
+      img: '-',
+      href: 'https://github.com/opendigg/awesome-github-vue',
+    },
+    {
+      title: 'JavaScript 库集合',
+      description: '很棒的 JavaScript 库，资源的集合。',
+      img: '-',
+      href: 'https://github.com/sorrycc/awesome-javascript',
+    },
+    // {
+    //   title: 'xxxxx',
+    //   description: 'xxxxx',
+    //   img: 'xxxxx',
+    //   href: 'xxxxx',
+    // },
+    // {
+    //   title: 'xxxxx',
+    //   description: 'xxxxx',
+    //   img: 'xxxxx',
+    //   href: 'xxxxx',
+    // },
+  ]
+}
